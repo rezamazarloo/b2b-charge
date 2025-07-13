29a7dec2d9315d1760df8a62f31cd4bfb5503301
Reza Mazarlou <rezamazarloo@yahoo.com>
2025-07-13 15:22:28 +0330

    started project apps

--- b2b/b2b/settings.py
+++ b2b/b2b/settings.py
@@ -42,6 +42,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # project apps
+    "finance",
+    "inventory",
+    "account",
+    "account.users",
 ]
 
 MIDDLEWARE = [
