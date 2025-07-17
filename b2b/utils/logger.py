ede45725ed7321e0ed9e551ad1ba7f5925252d4d
Reza Mazarlou <rezamazarloo@yahoo.com>
2025-07-17 16:29:56 +0330

    modified simcard logger methods from static nethod to class method also added request path to log

--- b2b/utils/logger.py
+++ b2b/utils/logger.py
@@ -1,33 +1,36 @@
 import logging
+from django.urls import reverse_lazy
 
 simcard_charge_logger = logging.getLogger("simcard_charge_logger")
 
 
 class SimCardChargeLogger:
-    @staticmethod
-    def log_start(user_id, amount, simcard_number):
+    ENDPOINT = reverse_lazy("inventory:simcard-charge")
+
+    @classmethod
+    def log_start(cls, user_id, amount, simcard_number):
         simcard_charge_logger.info(
-            f"[SIMCARD CHARGE START] - User: {user_id} | Amount: {amount} | SimCard: {simcard_number}"
+            f"{cls.ENDPOINT} | [SIMCARD CHARGE START] - User: {user_id} | Amount: {amount} | SimCard: {simcard_number}"
         )
 
-    @staticmethod
-    def log_success(user_id, amount, simcard_number, user_balance, simcard_balance, transaction_history_id):
+    @classmethod
+    def log_success(cls, user_id, amount, simcard_number, user_balance, simcard_balance, transaction_history_id):
         simcard_charge_logger.info(
-            f"[SIMCARD CHARGE SUCCESS] - User: {user_id} | Amount: {amount} | "
+            f"{cls.ENDPOINT} | [SIMCARD CHARGE SUCCESS] - User: {user_id} | Amount: {amount} | "
             f"SimCard: {simcard_number} | UserBalance: {user_balance} | "
             f"SimCardBalance: {simcard_balance} | TransactionHistoryID: {transaction_history_id}"
         )
 
-    @staticmethod
-    def log_validation_error(user_id, amount, simcard_number, error):
+    @classmethod
+    def log_validation_error(cls, user_id, amount, simcard_number, error):
         simcard_charge_logger.error(
-            f"[SIMCARD CHARGE VALIDATION ERROR] - User: {user_id} | Amount: {amount} | "
+            f"{cls.ENDPOINT} | [SIMCARD CHARGE VALIDATION ERROR] - User: {user_id} | Amount: {amount} | "
             f"SimCard: {simcard_number} | Error: {error}"
         )
 
-    @staticmethod
-    def log_system_error(user_id, amount, simcard_number, error):
+    @classmethod
+    def log_system_error(cls, user_id, amount, simcard_number, error):
         simcard_charge_logger.error(
-            f"[SIMCARD CHARGE SYSTEM ERROR] - User: {user_id} | Amount: {amount} | "
+            f"{cls.ENDPOINT} | [SIMCARD CHARGE SYSTEM ERROR] - User: {user_id} | Amount: {amount} | "
             f"SimCard: {simcard_number} | Error: {error}"
         )
